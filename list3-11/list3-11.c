122c6bcdfed607665255def7b187ba6da685b4d4
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-19 22:49:21 +0900

    Add files via upload

---
+++ list3-11/list3-11.c
@@ -0,0 +1,18 @@
+//読み込んだ2つの整数値の大きいほうの値を表示
+#include<stdio.h>
+
+int main(void)
+{
+	int n1, n2;
+
+	puts("2つの整数を入力せよ。");
+	printf("整数１："); scanf("%d", &n1);
+	printf("整数２："); scanf("%d", &n2);
+
+	if (n1 > n2)
+		printf("大きい方の値は%dです。\n", n1);
+	else
+		printf("大きい方の値は%dです。\n", n2);
+
+	return 0;
+}
