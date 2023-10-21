f6a1f5b69bab85683490ab2c652e9a706a87d5aa
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-22 08:04:21 +0900

    Add files via upload

---
+++ list4-13/list4-13.c
@@ -0,0 +1,18 @@
+//“Ç‚İ‚ñ‚¾®”‚ÌŒÂ”‚¾‚¯–‚ğ˜A‘±•\¦
+
+#include<stdio.h>
+
+int main(void)
+{
+	int no;
+
+	printf("³‚Ì®”F");
+	scanf("%d", &no);
+
+	int i;
+	for (i = 0; i <= no; i++)
+		putchar('*');
+	putchar('\n');
+
+	return 0;
+}
