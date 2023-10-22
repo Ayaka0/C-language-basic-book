0532599c0cebcecb6c780582a6d46d3e31c0bcaf
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-22 19:12:42 +0900

    Add files via upload

---
+++ list4-16/list4-16.c
@@ -0,0 +1,20 @@
+//読み込んだ整数値以下の偶数を表示
+
+#include<stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("整数値：");
+	scanf("%d", &n);
+
+	int i;
+	for (i = 2; i <= n; i += 2)
+		printf("%d ", i);
+	putchar('\n');
+
+
+	return 0;
+
+}
