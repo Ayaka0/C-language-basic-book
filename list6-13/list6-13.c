1b85cd58a85cbe8069924325e3d27eb3ecb6e874
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-26 20:52:52 +0900

    Add files via upload

---
+++ list6-13/list6-13.c
@@ -0,0 +1,44 @@
+//üŒ`’Tõ
+
+#include<stdio.h>
+
+#define NUMBER 5
+#define FAILED -1
+
+//—v‘f”n‚Ì”z—ñv‚©‚çkey‚Æˆê’v‚·‚é—v‘f‚ğ’Tõ
+int search(const int v[], int key, int n)
+{
+	int i = 0;
+
+	while (1) {
+		if (i == n)
+			return FAILED;	//’Tõ¸”s
+		if (v[i] == key)
+			return i;	//’Tõ¬Œ÷
+		i++;
+	}
+}
+
+int main(void)
+{
+	int ky, idx;
+	int x[NUMBER];
+
+	int i;
+	for (i = 0; i < NUMBER; i++) {
+		printf("x[%d] :", i);
+		scanf("%d", &x[i]);
+	}
+	printf("’T‚·’lF");
+	scanf("%d", &ky);
+
+	idx = search(x, ky, NUMBER); //—v‘f”NUMBER‚Ì”z—ñx‚©‚çy‚ğ’Tõ
+
+	if (idx == FAILED)
+		puts("’Tõ‚É¸”s‚µ‚Ü‚µ‚½B");
+	else
+		printf("%d‚Í%d”Ô–Ú‚É‚ ‚è‚Ü‚·B\n", ky, idx + 1);
+
+
+	return 0;
+}
