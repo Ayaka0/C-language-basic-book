24572278bcb6a1dc8f86f777e178bbcfcd539a0e
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-28 20:13:45 +0900

    Add files via upload

---
+++ list7-2/list7-2.c
@@ -0,0 +1,11 @@
+//’P‚È‚écharŒ^‚ª•„†‚Â‚«Œ^‚©•„†–³‚µŒ^‚©‚ğ”»’è
+
+#include<stdio.h>
+#include<limits.h>
+
+int main(void)
+{
+	printf("‚±‚Ìˆ—Œn‚ÌcharŒ^‚Í%s‚Å‚·\n", CHAR_MIN ? "•„†•t‚«Œ^" : "•„†‚È‚µŒ^");
+
+	return 0;
+}
