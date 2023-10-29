a501361c1a027a81cf6ac8ccedb893a09d4ef3df
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-29 19:23:18 +0900

    Add files via upload

---
+++ list9-1/list9-1.c
@@ -0,0 +1,12 @@
+//•¶Žš—ñƒŠƒeƒ‰ƒ‹‚Ì‘å‚«‚³‚ð•\Ž¦‚·‚é
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("sizeof(\"123\")		=%zu\n", sizeof("123"));
+	printf("sizeof(\"AB\\tC\")	=%zu\n", sizeof("AB\tC"));
+	printf("sizeof(\"abc\\0def\")	=%zu\n", sizeof("abc\0def"));
+
+	return 0;
+}
