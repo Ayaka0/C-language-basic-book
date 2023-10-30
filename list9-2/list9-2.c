958878f9a7ffba8b456ed89e185e60b57b40f897
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-30 21:17:20 +0900

    Add files via upload

---
+++ list9-2/list9-2.c
@@ -0,0 +1,17 @@
+//”z—ñ‚É•¶š—ñ‚ğŠi”[‚µ‚Ä•\¦
+
+#include<stdio.h>
+
+int main(void)
+{
+	char str[4];	//•¶š—ñ‚ğŠi”[‚·‚é”z—ñ
+
+	str[0] = 'A';
+	str[1] = 'B';
+	str[2] = 'C';
+	str[3] = '\0';
+
+	printf("•¶š—ñstr‚Í\"%s\"‚Å‚·B\n", str);
+
+	return 0;
+}
