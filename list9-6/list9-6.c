c9319aa85ebb04adae98e2ec417433679bc0c942
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-10-30 21:30:00 +0900

    Add files via upload

---
+++ list9-6/list9-6.c
@@ -0,0 +1,14 @@
+//•¶Žš—ñ‚Ì”z—ñ
+
+#include<stdio.h>
+
+int main(void)
+{
+	char s[][6] = { "Turbo", "NA", "DOHC" };
+
+	int i;
+	for (i = 0; i < 3; i++)
+		printf("s[%d] = \"%s\"\n", i, s[i]);
+
+	return 0;
+}
