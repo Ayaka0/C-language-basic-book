ea8945b8689c3c0b78ed03042d59c0f63c24c4aa
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-11-01 21:50:34 +0900

    Add files via upload

---
+++ list11-13/list11-13.c
@@ -0,0 +1,16 @@
+//atoiŠÖ”
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	char str[128];
+
+	printf("•¶š—ñ‚ğ“ü—ÍF");
+	scanf("%s", str);
+
+	printf("®”‚É•ÏŠ·‚·‚é‚Æ%d‚Å‚·B\n", atoi(str));
+
+	return 0;
+}
