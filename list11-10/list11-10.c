cede01743ab2b212a814b01001c09464ecf0c0c9
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-11-01 21:34:45 +0900

    Add files via upload

---
+++ list11-10/list11-10.c
@@ -0,0 +1,16 @@
+//strcpyŠÖ”‚Ì—˜—p—á
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char s1[10];
+	char *x = "XXXXXXXXX"; //9ŒÂ‚Ì'X'‚Æ‚È‚é•¶š
+
+	strcpy(s1, x); strncpy(s1, "12345", 3); printf("s1 = %s\n", s1);
+	strcpy(s1, x); strncpy(s1, "12345", 5); printf("s1 = %s\n", s1);
+	strcpy(s1, x); strncpy(s1, "12345", 7); printf("s1 = %s\n", s1);
+
+	return 0;
+}
