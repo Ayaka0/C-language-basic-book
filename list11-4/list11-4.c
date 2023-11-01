78c1a028aaae00c7394ae45f647aac0a22dd4610
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-11-01 20:58:25 +0900

    Add files via upload

---
+++ list11-4/list11-4.c
@@ -0,0 +1,18 @@
+//文字列の配列
+
+#include<stdio.h>
+
+int main(void)
+{
+	char a[][5] = { "LISP", "C", "Ada" }; //配列による文字列の配列
+	char* p[] = { "PAUL", "X", "MAC" }; //ポインタによる文字列の配列
+
+	int i;
+	for (i = 0; i < 3; i++)
+		printf("a[%d] = \"%s\"\n", i, a[i]);
+
+	for (i = 0; i < 3; i++)
+		printf("p[%d] = \"%s\"\n", i, p[i]);
+
+	return 0;
+}
