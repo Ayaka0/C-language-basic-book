05823a09ea671ff1e8eec2a16dabf0fffffaa346
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-11-07 21:33:35 +0900

    Add files via upload

---
+++ pra3-15/pra3-15.c
@@ -0,0 +1,17 @@
+//Šï”‚©‹ô”‚©‚Ì”»’è
+
+#include<stdio.h>
+
+int main(void)
+{
+	int no;
+	printf("®”‚ğ“ü—ÍF");
+	scanf("%d", &no);
+
+	if (no % 2)
+		printf("‚»‚Ì”‚ÍŠï”‚Å‚·B\n");
+	else
+		printf("‚»‚Ì”‚Í‹ô”‚Å‚·B\n");
+
+	return 0;
+}
