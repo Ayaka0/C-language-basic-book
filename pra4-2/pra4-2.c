f3cc5202b0f9da2e1e9b0dec48853602f040547d
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-11-08 21:14:17 +0900

    Add files via upload

---
+++ pra4-2/pra4-2.c
@@ -0,0 +1,34 @@
+//2‚Â‚Ì®”’l‚ğ“Ç‚İ‚İ˜a‚ğ‹‚ß‚éB
+
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b;
+	int lower, upper;
+
+	printf("®”a : "); scanf("%d", &a);
+	printf("®”b : "); scanf("%d", &b);
+
+	if (a > b) {
+
+		lower = b;
+		upper = a;
+	}
+	else {
+		lower = a;
+		upper = b;
+	}
+
+	int sum = 0;
+	int no = lower;
+
+	do {
+		sum = sum + no;
+		no = no + 1;
+	} while (no <= upper);
+
+	printf("%dˆÈã%dˆÈ‰º‚Ì‘S®”‚Ì˜a‚Í%d‚Å‚·\n", lower, upper, sum);
+
+	return 0;
+}
