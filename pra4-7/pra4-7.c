1de113a352fc8a920f99709c82d46608f019fba7
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-11-10 21:18:50 +0900

    Add files via upload

---
+++ pra4-7/pra4-7.c
@@ -0,0 +1,22 @@
+//®”‚Å‚Q‚Ì‚×‚«æ
+
+#include<stdio.h>
+
+int main(void)
+{
+	int no;
+	printf("®”‚ğ“ü—ÍF");
+	scanf("%d", &no);
+
+	int i = 2;
+
+	while (i <= no) {
+		printf("%d ", i);
+		i *= 2;
+	}
+
+	printf("\n");
+
+
+	return 0;
+}
